3baf0e26b83cce70619f32973ab538dd25b1f1b0
jmoreno206jm-droid <jmoreno.20.6jm@gmail.com>
2026-03-30 18:53:04 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,21 @@
-const CACHE_NAME = "adecco-asistencia-v2";
-
+const CACHE_NAME = 'asistencia-cache-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./js/html5-qrcode.min.js"
+  '/',
+  '/index.html',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// INSTALAR
-self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// ACTIVAR
-self.addEventListener("activate", e => {
-  self.clients.claim();
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
-      )
-    )
-  );
-});
-
-// FETCH (CACHE DINÁMICO)
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => {
-        return res || fetch(e.request).then(response => {
-          return caches.open(CACHE_NAME).then(cache => {
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        });
-      })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
